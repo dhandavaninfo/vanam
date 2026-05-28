ed952c7eddfbd61f4d908636ba10bdf9eac0b0e9
dhandavaninfo <dhandavan.info@gmail.com>
2026-05-28 14:12:08 +0530

    spring boot application

---
+++ src/main/java/com/van/spring/boot/vanam/VanamApplication.java
@@ -0,0 +1,13 @@
+package com.van.spring.boot.vanam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/van/spring/boot/vanam/VanamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.van.spring.boot.vanam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
